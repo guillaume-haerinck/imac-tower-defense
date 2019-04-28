b27f5b6dfcf69760059810e27f89b43e3a57e93e
JulesFouchy <45451201+JulesFouchy@users.noreply.github.com>
2019-04-28 23:56:25 +0200

    Add projectile factory

--- src/entity-factories/projectile-factory.hpp
+++ src/entity-factories/projectile-factory.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 #include "factory.hpp"
+#include "component-factories/sprite-factory.hpp"
 
 class ProjectileFactory : public Factory {
 public:
 	ProjectileFactory(entt::DefaultRegistry& registry);
+
+	void create(glm::vec2 initialPos, unsigned int targetId);
+
+private:
+	SpriteFactory m_spriteFactory;
 };
 
