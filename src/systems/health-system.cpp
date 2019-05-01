c07b0ff45210f5d9b1ab18451e52903e04ffa614
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-05-01 22:44:14 +0200

    Health bar done

--- src/systems/health-system.cpp
+++ src/systems/health-system.cpp
@@ -14,7 +14,7 @@ HealthSystem::HealthSystem(entt::DefaultRegistry& registry, EventEmitter& emitte
 	m_emitter.on<evnt::ProjectileHit>([this](const evnt::ProjectileHit & event, EventEmitter & emitter) {
 		cmpt::Health& health = m_registry.get<cmpt::Health>(event.targetId);
 		health.current -= event.damage;
-		if (health.current < 0) {
+		if (health.current <= 0.2f) {
 			m_emitter.publish<evnt::EnnemyDead>(event.position);
 			m_registry.destroy(event.targetId);
 		}
