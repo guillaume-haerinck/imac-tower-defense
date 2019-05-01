17d658d8f7ba756a7950e110252e8ddf01dc6041
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-05-01 21:45:19 +0200

    Limit construction

--- src/systems/construction-system.cpp
+++ src/systems/construction-system.cpp
@@ -15,10 +15,11 @@ ConstructionSystem::ConstructionSystem(entt::DefaultRegistry& registry, EventEmi
 		glm::vec2 tilePosition = this->m_map.projToGrid(event.mousePos.x, event.mousePos.y);
 		unsigned int entityId = this->m_map.getTile(tilePosition.x, tilePosition.y);
 		if (entityId != -1) {
-			//if (this->m_registry.has<tileTag::Constructible>(entityId)) {
-			cmpt::Transform trans = this->m_registry.get<cmpt::Transform>(entityId);
-			this->m_towerFactory.create(trans.position.x, trans.position.y);
-			//}
+			if (this->m_registry.has<tileTag::Constructible>(entityId)) {
+				cmpt::Transform trans = this->m_registry.get<cmpt::Transform>(entityId);
+				this->m_towerFactory.create(trans.position.x, trans.position.y);
+				this->m_registry.remove<tileTag::Constructible>(entityId);
+			}
 		}
 	});
 }
