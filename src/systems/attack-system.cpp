ad6af843fba94498420bc3b300711a84314abde6
JulesFouchy <45451201+JulesFouchy@users.noreply.github.com>
2019-05-04 12:50:44 +0200

    Towers keep their target until it dies or goes out of range

--- src/systems/attack-system.cpp
+++ src/systems/attack-system.cpp
@@ -7,8 +7,6 @@
 #include "components/targeting.hpp"
 #include "components/look-at.hpp"
 #include "components/shoot-at.hpp"
-#include "components/trigger.hpp"
-#include "components/transform.hpp"
 
 #include "services/locator.hpp"
 #include "services/debug-draw/i-debug-draw.hpp"
@@ -17,6 +15,7 @@ AttackSystem::AttackSystem(entt::DefaultRegistry& registry) : System(registry), 
 
 void AttackSystem::update() {
 
+	//Look at target
 	m_registry.view<cmpt::Targeting, cmpt::Transform , cmpt::LookAt>().each([this](auto entity, cmpt::Targeting & targeting, cmpt::Transform & transform, cmpt::LookAt lookAt) {
 		if ( m_registry.valid(targeting.targetId) ) {
 			glm::vec2 direction = m_registry.get<cmpt::Transform>(targeting.targetId).position - transform.position;
@@ -24,6 +23,7 @@ void AttackSystem::update() {
 		}
 	});
 
+	//Shoot at target
 	m_registry.view<cmpt::Targeting, cmpt::Transform, cmpt::ShootAt>().each([this](auto entity, cmpt::Targeting & targeting, cmpt::Transform & transform, cmpt::ShootAt & shootAt) {
 		if (m_registry.valid(targeting.targetId) ) {
 			if (shootAt.timer == shootAt.loadingTime) {
@@ -36,23 +36,32 @@ void AttackSystem::update() {
 		}
 	});
 
+	//Pick a new target if current one is out of range or dead
 	m_registry.view<cmpt::Transform, cmpt::Trigger, cmpt::Targeting>().each([this](auto entity1, cmpt::Transform & transform1, cmpt::Trigger & trigger1, cmpt::Targeting & targeting) {
 		IDebugDraw& dd = entt::ServiceLocator<IDebugDraw>::ref();
 		dd.DrawCircle(b2Vec2(transform1.position.x, transform1.position.y), trigger1.radius, b2Color(1, 0, 0, 0.5f));
 
-		m_registry.view<cmpt::Transform, cmpt::Trigger, entityTag::Enemy>().each([this, entity1, transform1, trigger1, &targeting](auto entity2, cmpt::Transform & transform2, cmpt::Trigger & trigger2, auto) {
-			const glm::vec2 deltaPos = transform2.position - transform1.position;
-			const float distanceSq = deltaPos.x * deltaPos.x + deltaPos.y * deltaPos.y;
-			const float radii = trigger1.radius + trigger2.radius;
-
-			if (distanceSq <= radii * radii) {
-				// TODO keep his target if still inside of the trigger
-				targeting.targetId = entity2;
-			} else {
-				if (targeting.targetId == entity2) {
-					targeting.targetId = -1;
+		if ( !m_registry.valid(targeting.targetId) || !isInRange(transform1, trigger1, targeting.targetId)) {
+			targeting.targetId = -1;
+			m_registry.view<cmpt::Transform, cmpt::Trigger, entityTag::Enemy>().each([this, entity1, transform1, trigger1, &targeting](auto entity2, cmpt::Transform & transform2, cmpt::Trigger & trigger2, auto) {
+				if ( isInRange(transform1,trigger1,transform2,trigger2) ) {
+					targeting.targetId = entity2;
 				}
-			}
-		});
+			});
+		}
 	});
+}
+
+
+bool AttackSystem::isInRange(cmpt::Transform transform1, cmpt::Trigger trigger1, cmpt::Transform transform2, cmpt::Trigger trigger2) {
+	const glm::vec2 deltaPos = transform2.position - transform1.position;
+	const float distanceSq = deltaPos.x * deltaPos.x + deltaPos.y * deltaPos.y;
+	const float radii = trigger1.radius + trigger2.radius;
+	return distanceSq <= radii * radii;
+}
+
+bool AttackSystem::isInRange(cmpt::Transform transform1, cmpt::Trigger trigger1, unsigned int targetId) {
+	cmpt::Transform transform2 = m_registry.get<cmpt::Transform>(targetId);
+	cmpt::Trigger trigger2 = m_registry.get<cmpt::Trigger>(targetId);
+	return isInRange(transform1, trigger1, transform2, trigger2);
 }
--- src/systems/attack-system.hpp
+++ src/systems/attack-system.hpp
@@ -3,10 +3,15 @@
 #include "system.hpp"
 #include "entity-factories/projectile-factory.hpp"
 
+#include "components/trigger.hpp"
+#include "components/transform.hpp"
+
 class AttackSystem : public System {
 public:
 	AttackSystem(entt::DefaultRegistry& registry);
 	void update();
 private:
 	ProjectileFactory m_projectileFactory;
+	bool isInRange(cmpt::Transform transform1, cmpt::Trigger trigger1, cmpt::Transform transform2, cmpt::Trigger trigger2);
+	bool isInRange(cmpt::Transform transform1, cmpt::Trigger trigger1, unsigned int targetId);
 };
