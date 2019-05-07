960f40ea32b3ce2303782d00f570ff32688b05ea
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-05-07 19:42:43 +0200

    System class  handles input subscription

---
+++ src/systems/i-system.cpp
@@ -0,0 +1,61 @@
+#include "i-system.hpp"
+
+void ISystem::connectInputs() {
+	if (m_bConnected == false) {
+		auto conMouseMove = m_emitter.on<evnt::MouseMove>([this](const evnt::MouseMove & event, EventEmitter & emitter) {
+			this->onMouseMove(event);
+		});
+		m_mouseMove = std::make_unique<entt::Emitter<EventEmitter>::Connection<evnt::MouseMove>>(conMouseMove);
+
+
+		auto conLeftClickUp = m_emitter.on<evnt::LeftClickUp>([this](const evnt::LeftClickUp & event, EventEmitter & emitter) {
+			this->onLeftClickUp(event);
+		});
+		m_leftClickUp = std::make_unique<entt::Emitter<EventEmitter>::Connection<evnt::LeftClickUp>>(conLeftClickUp);
+
+
+		auto conLeftClickDown = m_emitter.on<evnt::LeftClickDown>([this](const evnt::LeftClickDown & event, EventEmitter & emitter) {
+			this->onLeftClickDown(event);
+		});
+		m_leftClickDown = std::make_unique<entt::Emitter<EventEmitter>::Connection<evnt::LeftClickDown>>(conLeftClickDown);
+
+
+		auto conRightClickUp = m_emitter.on<evnt::RightClickUp>([this](const evnt::RightClickUp & event, EventEmitter & emitter) {
+			this->onRightClickUp(event);
+		});
+		m_rightClickUp = std::make_unique<entt::Emitter<EventEmitter>::Connection<evnt::RightClickUp>>(conRightClickUp);
+
+
+		auto conRightClickDown = m_emitter.on<evnt::RightClickDown>([this](const evnt::RightClickDown & event, EventEmitter & emitter) {
+			this->onRightClickDown(event);
+		});
+		m_rightClickDown = std::make_unique<entt::Emitter<EventEmitter>::Connection<evnt::RightClickDown>>(conRightClickDown);
+	}
+}
+
+void ISystem::disconnectInputs() {
+	if (m_bConnected == true) {
+		m_emitter.erase(*m_mouseMove);
+		m_mouseMove.reset();
+
+		m_emitter.erase(*m_leftClickUp);
+		m_leftClickUp.reset();
+
+		m_emitter.erase(*m_leftClickDown);
+		m_leftClickDown.reset();
+
+		m_emitter.erase(*m_rightClickUp);
+		m_rightClickUp.reset();
+
+		m_emitter.erase(*m_rightClickDown);
+		m_rightClickDown.reset();
+	}
+}
+
+void ISystem::onMouseMove(const evnt::MouseMove& event) {}
+
+void ISystem::onLeftClickUp(const evnt::LeftClickUp& event) {}
+void ISystem::onLeftClickDown(const evnt::LeftClickDown& event) {}
+
+void ISystem::onRightClickUp(const evnt::RightClickUp& event) {}
+void ISystem::onRightClickDown(const evnt::RightClickDown& event) {}
--- src/systems/i-system.hpp
+++ src/systems/i-system.hpp
@@ -1,15 +1,42 @@
 #pragma once
 
 #include <entt/entt.hpp>
+#include <memory>
+
+#include "events/handlers/event-emitter.hpp"
+#include "events/left-click-up.hpp"
+#include "events/left-click-down.hpp"
+#include "events/right-click-up.hpp"
+#include "events/right-click-down.hpp"
+#include "events/mouse-move.hpp"
 
 class ISystem {
 public:
-	virtual void connectEvents() = 0;
-	virtual void disconnectEvents() = 0;
 	virtual void update(float deltatime) = 0;
+	
+	// Input events
+	void connectInputs();
+	void disconnectInputs();
+
+	virtual void onMouseMove(const evnt::MouseMove& event);
+
+	virtual void onLeftClickUp(const evnt::LeftClickUp& event);
+	virtual void onLeftClickDown(const evnt::LeftClickDown& event);
+
+	virtual void onRightClickUp(const evnt::RightClickUp& event);
+	virtual void onRightClickDown(const evnt::RightClickDown& event);
 
 protected:
-	ISystem(entt::DefaultRegistry& registry) : m_registry(registry), m_bConnected(false) {}
+	ISystem(entt::DefaultRegistry& registry, EventEmitter& emitter) : m_registry(registry), m_emitter(emitter), m_bConnected(false) {}
+
 	entt::DefaultRegistry& m_registry;
+	EventEmitter& m_emitter;
 	bool m_bConnected;
+
+private:
+	std::unique_ptr<entt::Emitter<EventEmitter>::Connection<evnt::LeftClickUp>> m_leftClickUp;
+	std::unique_ptr<entt::Emitter<EventEmitter>::Connection<evnt::LeftClickDown>> m_leftClickDown;
+	std::unique_ptr<entt::Emitter<EventEmitter>::Connection<evnt::RightClickUp>> m_rightClickUp;
+	std::unique_ptr<entt::Emitter<EventEmitter>::Connection<evnt::RightClickUp>> m_rightClickDown;
+	std::unique_ptr<entt::Emitter<EventEmitter>::Connection<evnt::MouseMove>> m_mouseMove;
 };
