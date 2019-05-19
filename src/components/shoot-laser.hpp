8030159a5f98ab4f8b26e313c0f383ce35dd518f
Jules Fouchy <jules.fouchy@ntymail.com>
2019-05-19 18:21:46 +0200

    Lasers no longer have random colors

--- src/components/shoot-laser.hpp
+++ src/components/shoot-laser.hpp
@@ -7,7 +7,7 @@ namespace cmpt {
 	struct ShootLaser {
 		ShootLaser(bool isActiv=false) : isActiv(isActiv) {
 			IRandom& random = entt::ServiceLocator<IRandom>::ref();
-			col = glm::vec3(122 + random.random(-25, 25), 249 + random.random(-25, 25), 237 + random.random(-75, 75));
+			col = glm::vec3(122, 249, 237);
 		}
 		bool isActiv;
 		glm::vec3 col;
